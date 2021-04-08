071afdecdbcdcdee4a13e971b5aec0f217fc2791
sqnguyen820 <sqnguyen829@gmail.com>
2021-04-08 16:46:49 -0500

    first non-repeating char

---
+++ 10_first_non_repeating_character.js
@@ -0,0 +1,32 @@
+function firstNonRepeatingLetter(s) {
+
+    let letters = {}
+    
+    for(let i=0; i<s.length; i++){
+      if(letters[s[i].toLowerCase()]){
+        letters[s[i].toLowerCase()]++
+      }else{
+        letters[s[i].toLowerCase()] = 1
+      }
+    }
+    
+    for(let i=0; i<s.length; i++){
+      if(letters[s[i].toLowerCase()]<2){
+        return s[i]
+      }
+    }
+    
+    return ''
+    
+  }
+
+//   Write a function named first_non_repeating_letter that takes a string input, and returns the 
+// first character that is not repeated anywhere in the string.
+
+//   For example, if given the input 'stress', the function should return 't', since the letter t only 
+// occurs once in the string, and occurs first in the string.
+  
+//   As an added challenge, upper- and lowercase letters are considered the same character, but the function 
+// should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+  
+//   If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
